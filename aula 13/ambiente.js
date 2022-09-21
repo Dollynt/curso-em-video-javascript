8d1e70a7ebc3dc408eaed1c1d7426cc7df3413b5
Luiz Guylherme <86082879+Dollynt@users.noreply.github.com>
2022-09-21 14:39:26 -0300

    new file:   aula 13/ambiente.js new file:   aula 14/ambiente.js

---
+++ aula 13/ambiente.js
@@ -0,0 +1,14 @@
+var counter = 1
+
+// do test code
+do {
+    console.log(`Salve!! Passo ${counter}`)
+    counter++
+} while (counter < 6)
+
+
+//while test code
+/*while (counter < 6) {
+    console.log(`Salve!! Passo ${counter}`)
+    counter++
+}*/
---
+++ aula 14/ambiente.js
@@ -0,0 +1,4 @@
+
+for(var counter = 1;counter <= 10;counter++) {
+    console.log(counter)
+}
